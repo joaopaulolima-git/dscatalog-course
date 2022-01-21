a00ba2e1c63f5c2a5ed25ac911e8c722f14a7b7f
João Paulo <jplopapa.git@gmail.com>
2022-01-21 16:18:59 -0300

    Auditing data

--- backend/src/main/java/com/jpdev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jpdev/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jpdev.dscatalog.dto.CategoryDTO;
 import com.jpdev.dscatalog.entities.Category;
 import com.jpdev.dscatalog.repositories.CategoryRepository;
+import com.jpdev.dscatalog.services.exceptions.DatabaseException;
 import com.jpdev.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -53,5 +56,18 @@ public class CategoryService {
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);			
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 }
